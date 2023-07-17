16a82b5ce081c39201044f1a08b8d70777821ef8
Raymond Fajiculay <raymondfajiculay@Raymond.local>
2023-07-17 22:25:37 +0800

    add

---
+++ js/main.js
@@ -0,0 +1,61 @@
+/*--------------------------------------------------/*
+ * Nav Bar
+/*--------------------------------------------------*/
+const hamburger = document.querySelector(".hamburger ");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-item").forEach((n) =>
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
+);
+
+/*--------------------------------------------------/*
+ * Accordion
+/*--------------------------------------------------*/
+
+const contentBox = document.querySelectorAll(".contentBox");
+const content = document.querySelectorAll(".content");
+
+contentBox.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        content.forEach((acc) => {
+            if (acc.classList.contains("active")) {
+                acc.classList.remove("active");
+                contentBox.forEach((btn) => {
+                    btn.classList.remove("active");
+                    btn.firstElementChild.firstElementChild.classList.remove(
+                        "active"
+                    );
+                });
+            } else {
+                const panel = btn.lastElementChild;
+                panel.classList.toggle("active");
+                btn.classList.toggle("active");
+                console.log(panel);
+                console.log(btn);
+
+                const icon = btn.firstElementChild.firstElementChild;
+                icon.classList.toggle("active");
+                console.log(icon);
+            }
+        });
+    });
+});
+
+/*--------------------------------------------------/*
+ * Readmore
+/*--------------------------------------------------*/
+
+const rdbtn = document.querySelector(".rdBtn");
+const toShow = document.querySelector(".readmore");
+
+rdbtn.addEventListener("click", () => {
+    toShow.classList.toggle("active");
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Door Ringer
+
+## Introduction
+This project is a responsive front-end implementation of a Door Ringer Website. It provides a user-friendly interface for placing sports bets and managing user accounts. The front-end is built using **HTML**, **CSS**, and **JavaScript**.
+
+## Features
+- **Responsive Design**: The front-end is optimized for various screen sizes, ensuring a consistent user experience across different devices.
+- **User-Friendly Interface**: The application provides an intuitive and easy-to-use interface, allowing users to place bets and manage their accounts effortlessly.
+
+## Technologies Used
+- **HTML**: The structure and layout of the application are created using HTML.
+- **CSS**: Custom CSS styles are applied to enhance the visual appeal and user experience.
+- **JavaScript**: Interactive features and functionality are implemented using JavaScript.
